f110298c094f4e71436194cee263b4c188148f9e
nampole <nampole@aol.com>
2018-10-11 11:00:20 +0900

    Initial commit

---
+++ app/src/main/java/com/hfad/duck/MainActivity.java
@@ -0,0 +1,37 @@
+package com.hfad.duck;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClicked(android.view.View view) {
+//        버튼을 클릭하면 수행됨
+    }
+
+    public void onToggleButtonClicked (android.view.View view) {
+        boolean tbon = ((ToggleButton) view).isChecked();
+            if (tbon) {
+//            on 의 값이 true 일 경우
+            }
+            else {
+//            on 의 값이 false 일 경우
+            }
+    }
+
+    public void  onSwitchClicked (android.view.View view) {
+        boolean son = ((Switch) view).isChecked();
+        if (son) {
+//            on 의 값이 true 일 경우
+        }
+        else {
+//            on 의 값이 false 일 경우
+        }
+    }
+}
